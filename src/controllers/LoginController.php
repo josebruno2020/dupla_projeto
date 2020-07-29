8a31e6a75b0fedf9059cf910e90709e74cc2a63e
José Bruno <josebrunocampanholi@gmail.com>
2020-07-29 15:56:40 -0300

    3 tentivas no login. Se não bloqueia e o usuario tem que esperar por cinco minutos!

--- src/controllers/LoginController.php
+++ src/controllers/LoginController.php
@@ -7,7 +7,7 @@ class LoginController extends Controller {
             $flash = $_SESSION['flash'];
             $_SESSION['flash'] = '';
         }
-
+        
         $this->render('login', [
             'flash' => $flash
         ]);
@@ -15,25 +15,47 @@ class LoginController extends Controller {
     }
 
     public function loginAction(){
+        date_default_timezone_set('America/Sao_Paulo');
         $usuarios = new Usuarios();
         $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
         $senha = filter_input(INPUT_POST, 'senha');
+        
+        //Verificando se existe uma sessão com as tentativas;
+        if(isset($_SESSION['tentativa']) && $_SESSION['tentativa'] >= 3){
+            $hora2 = date('H:i:s', strtotime('+5 minutes'));
+            $tempo_transcorrido = (strtotime($hora2) - strtotime($_SESSION['hora']));
+            echo $tempo_transcorrido;
+            $_SESSION['flash'] = 'Tentou mais de 3 vezes! Bloqueado até: '.$hora2;
+            if($tempo_transcorrido >= 600){
+                unset($_SESSION['tentativa']);
+                $_SESSION['flash'] = 'Tentativas zeradas!';
+            }
+            
+            header("Location: ".BASE_URL);
 
-        if($email && $senha){
-            if($usuarios->fazerLogin($email, $senha) == true){
+        } else{
 
-                header("Location: ".BASE_URL);
-
-            } else{
-
-                $_SESSION['flash'] = 'Usuário e/ou Senha inválidos!';
+            if($email && $senha){
+                //Verifica os dados para fazer o Login;
+                if($usuarios->fazerLogin($email, $senha) == true){
+                    header("Location: ".BASE_URL);
+    
+                } else{
+                    //Criar uma contagem de vezes que a pessoa pode digitar os campos;
+                    if(!isset($_SESSION['tentativa'])){
+                        $_SESSION['tentativa'] = 0;
+                    }
+                    $_SESSION['tentativa']++;
+                    $_SESSION['flash'] = 'Usuário e/ou Senha inválidos! Tentativa: '.$_SESSION['tentativa'];
+                    //Definindo a hora da tentativa;
+                    $_SESSION['hora'] = date('H:i:s');
+                    header("Location: ".BASE_URL."login");
+                }
+    
+            } else {
+                $_SESSION['flash'] = 'Preencha todos os campos!';
                 header("Location: ".BASE_URL."login");
             }
-
-        } else {
-            $_SESSION['flash'] = 'Preencha todos os campos!';
-            header("Location: ".BASE_URL."login");
         }
     }
-
 }
